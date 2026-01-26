8fce545c760990b02060f22a8bc3f541cf6f84f8
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-01-26 14:19:17 +0530

    chore: add lazysql keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,5 +8,16 @@ vim.keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
 -- M: remove the project root given by lsp
 vim.g.root_spec = { { ".git", "lua" }, "cwd" }
 
--- M: lazydocker keymap
+-- M: open lazydocker keymap
 vim.keymap.set("n", "<leader>kd", ":term lazydocker<CR>i", { desc = "LazyDocker" })
+
+-- M: open lazysql keymap
+vim.keymap.set("n", "<leader>kp", function()
+  require("snacks").terminal("lazysql", {
+    win = {
+      style = "float",
+      width = 0.9,
+      height = 0.9,
+    },
+  })
+end, { desc = "Postgres TUI (Lazysql)" })
