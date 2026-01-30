a7483c33fe49cd01639cc3d39c139aaf5f7b6431
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-01-30 19:20:40 +0530

    chore: add autocmd to autosave on focuschange and runs rustfmt on each autosave

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,8 @@
+-- M: autosave on focus change and each autosave triggers rustfmt
 vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave", "CmdlineEnter" }, {
   desc = "Autosave on focus change",
   pattern = "*",
+  nested = true,
   callback = function()
     -- Check if buffer is modified, has a filename, and is a normal file type
     if vim.bo.modified and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
