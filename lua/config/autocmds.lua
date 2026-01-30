b5351bf4e2d461580538f504effff9f09a443546
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-01-30 18:15:53 +0530

    chore: add autosave command (rustfmt doesn't work)

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,2 +1,10 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave", "CmdlineEnter" }, {
+  desc = "Autosave on focus change",
+  pattern = "*",
+  callback = function()
+    -- Check if buffer is modified, has a filename, and is a normal file type
+    if vim.bo.modified and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+      vim.cmd("silent! update")
+    end
+  end,
+})
