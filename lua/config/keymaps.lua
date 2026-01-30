5c3c03316d2f2e3dd1e9c9350fd72eff8c4b720e
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-01-30 17:51:07 +0530

    chore: remove unnecessary configs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,19 +5,5 @@
 -- M: in normal mode ; will funtion as :
 vim.keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
 
--- M: remove the project root given by lsp
-vim.g.root_spec = { { ".git", "lua" }, "cwd" }
-
 -- M: open lazydocker keymap
 vim.keymap.set("n", "<leader>kd", ":term lazydocker<CR>i", { desc = "LazyDocker" })
-
--- M: open lazysql keymap
-vim.keymap.set("n", "<leader>kp", function()
-  require("snacks").terminal("lazysql", {
-    win = {
-      style = "float",
-      width = 0.9,
-      height = 0.9,
-    },
-  })
-end, { desc = "Postgres TUI (Lazysql)" })
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,9 +1,9 @@
--- M: to enable <C-j> in terminal which allows to run multiline commands in nvim terminal
 return {
   {
     "folke/snacks.nvim",
-    opts = {
-      terminal = {
+    opts = function(_, opts)
+      -- M: to enable <C-j> in terminal which allows to run multiline commands in nvim terminal
+      opts.terminal = {
         win = {
           keys = {
             -- Set the navigation keys to false to disable them inside the terminal
@@ -13,7 +13,18 @@ return {
             nav_l = false,
           },
         },
-      },
-    },
+      }
+
+      -- M: s command in dashboard restores the last opened project
+      for _, key in ipairs(opts.dashboard.preset.keys) do
+        if key.key == "s" then
+          -- 1. Unset the default internal session handler
+          key.section = nil
+          -- 2. Clean command without the <cr> syntax error
+          key.action = ":lua require('persistence').load({ last = true })"
+          break
+        end
+      end
+    end,
   },
 }
