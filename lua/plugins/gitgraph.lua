55f09d6b0309da5ab0c20f405c264b3dce3d1028
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-02-09 18:04:29 +0530

    feat: add option to checkout when we hit enter in gitgraph

--- lua/plugins/gitgraph.lua
+++ lua/plugins/gitgraph.lua
@@ -25,9 +25,30 @@ return {
         fields = { "hash", "timestamp", "author", "branch_name", "tag" },
       },
       hooks = {
+        -- IMPACT: When you hit Enter on a commit, you now get a choice
         on_select_commit = function(commit)
-          vim.notify("DiffviewOpen " .. commit.hash .. "^!")
-          vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
+          vim.ui.select({ "Diffview Open", "Checkout Commit" }, {
+            prompt = "Select Action:",
+            format_item = function(item)
+              return "➜ " .. item
+            end,
+          }, function(choice)
+            if not choice then
+              return
+            end
+
+            if choice == "Diffview Open" then
+              vim.notify("DiffviewOpen " .. commit.hash .. "^!")
+              vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
+            elseif choice == "Checkout Commit" then
+              -- Perform the checkout
+              vim.cmd(":!git checkout " .. commit.hash)
+              -- Optional: Notify the user
+              vim.notify("Checked out " .. commit.hash, vim.log.levels.INFO)
+              -- Optional: Redraw the graph to show updated HEAD
+              require("gitgraph").draw({}, { all = true, max_count = 5000 })
+            end
+          end)
         end,
         on_select_range_commit = function(from, to)
           vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
@@ -38,12 +59,11 @@ return {
     config = function(_, opts)
       require("gitgraph").setup(opts)
 
-      -- Auto-command to configure the gitgraph buffer correctly
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "gitgraph",
         callback = function()
           vim.opt_local.buflisted = false
-          vim.opt_local.buftype = "nofile" -- Prevents "Overwrite existing file" popup
+          vim.opt_local.buftype = "nofile"
           vim.keymap.set("n", "q", "<cmd>bd<cr>", { buffer = true, desc = "Close GitGraph" })
         end,
       })
